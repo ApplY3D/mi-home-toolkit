20f44d75d89ce5fde558ee6d4a9dbe7a1900489b
Dmitrii Kuzmin <kuzmin_dmitrii@inbox.ru>
2024-04-21 09:53:11 +0400

    feat(tauri): http

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,12 +3,18 @@
 
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn greet(name: &str) -> Result<String, ()> {
+    use tauri_plugin_http::reqwest;
+    let res = reqwest::get("https://google.com").await;
+    Ok(format!(
+        "Hello, {}! You've been greeted from Rust!",
+        res.is_ok()
+    ))
 }
 
 fn main() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
